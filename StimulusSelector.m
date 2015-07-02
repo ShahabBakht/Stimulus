bfeea09aead48b53f2e2d7090a6461831861befe
ShahabBakht <shahab.bakhtiari@gmail.com>
2015-07-02 10:44:35 -0400

    created StimulusSelector

---
+++ StimulusSelector.m
@@ -0,0 +1,20 @@
+
+%% select the type of stimulus here
+
+S.Type = ...
+    'StepRamp';
+%     'DoubleStepRamp';
+%     'Sinusoidal';
+%     'TargetSelection';
+
+
+%%
+
+ParameterFileLocation = './ParameterFiles/';
+ParameterFileName = [S.Type,'ParameterFile.m'];
+
+run([ParameterFileLocation,ParameterFileName]);
+
+%%
+
+    
