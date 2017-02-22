2b7b05449fef8563d22fde5d95c7327c4f2cf1d1
Shahab <Shahab@132.216.57.253>
2017-02-22 13:02:03 -0500

    modified Foreage code

--- Foreage.m
+++ Foreage.m
@@ -1,15 +1,15 @@
 function result=Foreage
 
-% Short MATLAB example program that uses the Eyelink and Psychophysics
-% Toolboxes to measure viewing behaviour for an image.
-%
-% History
-% 15-06-10  fwc     created it, based on EyelinkExample.m
+
 
 
 commandwindow;
 S.ScreenCov_v = 0.4571;
 S.ScreenCov_h = 0.3556;
+S.PPD_X = 15;
+S.PPD_Y = 15;
+S.numTrials = 2;
+
 try
     
     fprintf('EyelinkToolbox Image View Example\n\n\t');
@@ -49,6 +49,9 @@ try
     el=EyelinkInitDefaults(window);
     el.calibrationtargetsize= 1;
     el.calibrationtargetwidth=0.5;
+    el.calibrationtargetcolour=WhiteIndex(el.window);
+    el.msgfontcolour = WhiteIndex(el.window);
+    el.backgroundcolour = BlackIndex(el.window);
     
     EyelinkUpdateDefaults(el);
     
@@ -77,7 +80,7 @@ try
     end
     
     Eyelink('command','screen_pixel_coords = %ld %ld %ld %ld', 0, 0, winWidth-1, winHeight-1);
-    Eyelink('command', 'calibration_type = HV9');
+    Eyelink('command', 'calibration_type = HV13');
     Eyelink('command', 'generate_default_targets = YES');
     
     Eyelink('command', 'button_function 5 "accept_target_fixation"');
@@ -106,6 +109,22 @@ try
 
     % do a final check of calibration using driftcorrection
     % You have to hit esc before return.
+    
+    % Location of the image on the screen
+    windowSubPart = [winWidth/2 - wRect(3) * S.ScreenCov_h/2, winHeight/2 - wRect(4) * S.ScreenCov_v/2, ...
+        winWidth/2 + wRect(3) * S.ScreenCov_h/2,  winHeight/2 + wRect(4) * S.ScreenCov_v/2];
+    % Location of the target
+    rangeX = windowSubPart(3) - windowSubPart(1);
+    rangeY = windowSubPart(4) - windowSubPart(2);
+    smallestX = windowSubPart(1);
+    smallestY = windowSubPart(2);
+    meanTargetLocation_x = rangeX * rand + smallestX;
+    meanTargetLocation_y = rangeY * rand + smallestY;
+    stdTargetLocation_x = 0;
+    stdTargetLocation_y = 0;
+    
+    
+    for trcount = 1:S.numTrials
     EyelinkDoDriftCorrection(el);
     
         
@@ -121,19 +140,31 @@ try
     myimg='konijntjes1024x768.jpg';
     imdata=imread(myimg);
     imtex=Screen('MakeTexture', el.window, imdata);
-    windowSubPart = [winWidth/2 - wRect(3) * S.ScreenCov_h/2, winHeight/2 - wRect(4) * S.ScreenCov_v/2, ...
-        winWidth/2 + wRect(3) * S.ScreenCov_h/2,  winHeight/2 + wRect(4) * S.ScreenCov_v/2];
-%     windowSubPart = [winWidth/2 - 100, winHeight/2 - 50, 200, 100];
+    
+    
+    % Show the image
     result.windowSubPart = windowSubPart;
-    Screen('FillRect', el.window, el.backgroundcolour);
+    Screen('FillRect', el.window, [0 0 0]);
     Screen('DrawTexture', el.window, imtex, [], windowSubPart);  % fill screen with image
     
-    % Some useful info for user about what's happening.
-    [width, height]=Screen('WindowSize', el.window);
-
-    Screen('TextFont', el.window, el.msgfont);
-    Screen('TextSize', el.window, el.msgfontsize);
-    Screen('DrawText', el.window, 'Just look around for a while.', 200, height-el.msgfontsize-20, el.msgfontcolour);
+    
+    
+    thisTargetLocation = normrnd([meanTargetLocation_x,meanTargetLocation_y],...
+        [stdTargetLocation_x,stdTargetLocation_y]);
+    result.thisTargetLocation = thisTargetLocation;
+    % if target should be visible uncomment below lines
+    % Screen('FillOval',el.window, [0, 0 , 0], ...
+    %   [thisTargetLocation(1)-5,thisTargetLocation(2)-5,thisTargetLocation(1)+5,thisTargetLocation(2)+5]);
+    % Screen('FillOval',el.window, [255, 255 , 255], ...
+    %    [thisTargetLocation(1)-2,thisTargetLocation(2)-2,thisTargetLocation(1)+2,thisTargetLocation(2)+2]);
+    
+    
+%     % Some useful info for user about what's happening.
+%     [width, height]=Screen('WindowSize', el.window);
+% 
+%     Screen('TextFont', el.window, el.msgfont);
+%     Screen('TextSize', el.window, el.msgfontsize);
+%     Screen('DrawText', el.window, 'Just look around for a while.', 200, height-el.msgfontsize-20, el.msgfontcolour);
 
     % Show result on screen:
     Screen('Flip', el.window)
@@ -144,8 +175,9 @@ try
     mx = [];
     my = [];
     startTime = GetSecs;
+    initDetectionTime = GetSecs;
     while GetSecs < startTime + 20
-%         WaitSecs(3);
+
     % Query  eyetracker") -
     % (mx,my) is our gaze position.
     
@@ -162,17 +194,29 @@ try
                 
                 mx=[mx,x];
                 my=[my,y];
+                
+                
+                    
+                    if abs(mx(end) - thisTargetLocation(1))<=10 && abs(my(end) - thisTargetLocation(2))<=10
+                        timer = GetSecs - initDetectionTime;
+                        if timer >= 0.05
+                            initDetectionTime = GetSecs;
+                            break
+                            
+                        end
+                    
+                    end
+                
             end
         end
     end
     
     end
-    result.mx = mx;
-    result.my = my;
+    
     
     % STEP 7 remove image
-    Screen('FillRect', el.window, el.backgroundcolour);
-    Screen('DrawText', el.window, 'Done', width/2, height/2, el.msgfontcolour);
+    Screen('FillRect', el.window, [0 0 0]);
+    Screen('DrawText', el.window, 'Well done!', winWidth/2, winHeight/2, el.msgfontcolour);
     Screen('Flip', el.window);
     % mark image removal time in data file
     Eyelink('Message', 'ENDTIME');
@@ -183,6 +227,14 @@ try
     % finish up: stop recording eye-movements, 
     % close graphics window, close data file and shut down tracker
     Eyelink('StopRecording');
+    
+    X{trcount} = mx;
+    Y{trcount} = my;
+    
+    end
+    result.eyeX = X;
+    result.eyeY = Y;
+    
     Eyelink('CloseFile');    
     
     try
